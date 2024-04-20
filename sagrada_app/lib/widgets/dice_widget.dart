563349f9e4b49daf2565b67cef72b551ef817c8e
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-04-20 19:20:10 +0200

    Beautify dice widget

--- sagrada_app/lib/widgets/dice_widget.dart
+++ sagrada_app/lib/widgets/dice_widget.dart
@@ -5,16 +5,17 @@ import 'package:sagrada_app/widgets/dice_color_extension.dart';
 class DiceWidget extends StatefulWidget {
   const DiceWidget({super.key});
 
-  final double size = 100.0;
+  double get size => 100.0;
+  double get dotDiameter => size / 4;
+  double get dotRadius => dotDiameter / 2;
 
   @override
   State<DiceWidget> createState() => _DiceWidgetState();
 }
 
 class _DiceWidgetState extends State<DiceWidget> {
-  Dice _dice = const Dice(value: 1, color: DiceColor.red);
-  double _rotationX = 0;
-  double _rotationY = 0;
+  int value = 1;
+  DiceColor? color;
 
   @override
   Widget build(BuildContext context) {
@@ -27,32 +28,40 @@ class _DiceWidgetState extends State<DiceWidget> {
         width: widget.size,
         height: widget.size,
         decoration: BoxDecoration(
-          color: _dice?.color.color ?? Colors.grey,
+          color: color?.color ?? Colors.grey,
           borderRadius: BorderRadius.circular(widget.size / 8),
           backgroundBlendMode: BlendMode.modulate,
         ),
-        transform: Matrix4.rotationX(_rotationX)..rotateY(_rotationY),
         alignment: Alignment.center,
-        child: Center(
-          child: Text(
-            _dice?.value.toString() ?? '',
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+        child: color != null
+            ? _DotsStackWidget(
+                size: widget.size,
+                diameter: widget.dotDiameter,
+                count: value,
+              )
+            : const SizedBox.shrink(),
       ),
     );
   }
 
-  DiceColor _getNextColor(DiceColor color) {
-    return DiceColor.values[(color.index + 1) % DiceColor.values.length];
+  DiceColor? _getNextColor(DiceColor? color) {
+    if (color == null) {
+      return DiceColor.values[0];
+    }
+    if (color == DiceColor.values.last) {
+      return null;
+    }
+    return DiceColor.values[(color.index + 1)];
   }
 
-  DiceColor _getPreviousColor(DiceColor color) {
-    return DiceColor.values[
-        (color.index - 1 + DiceColor.values.length) % DiceColor.values.length];
+  DiceColor? _getPreviousColor(DiceColor? color) {
+    if (color == null) {
+      return DiceColor.values.last;
+    }
+    if (color == DiceColor.values.first) {
+      return null;
+    }
+    return DiceColor.values[(color.index - 1)];
   }
 
   int _getNextValue(int value) {
@@ -63,32 +72,18 @@ class _DiceWidgetState extends State<DiceWidget> {
     return value == 1 ? 6 : value - 1;
   }
 
-  void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    setState(() {
-      _rotationY += details.primaryDelta! / 100;
-    });
-  }
+  void _onHorizontalDragUpdate(DragUpdateDetails details) {}
 
-  void _onVerticalDragUpdate(DragUpdateDetails details) {
-    setState(() {
-      _rotationX -= details.primaryDelta! / 100;
-    });
-  }
+  void _onVerticalDragUpdate(DragUpdateDetails details) {}
 
   void _onHorizontalDragEnd(DragEndDetails details) {
     if (details.velocity.pixelsPerSecond.dx > 0) {
       setState(() {
-        _dice = Dice(
-          value: _getPreviousValue(_dice!.value),
-          color: _dice!.color,
-        );
+        value = _getPreviousValue(value);
       });
     } else {
       setState(() {
-        _dice = Dice(
-          value: _getNextValue(_dice!.value),
-          color: _dice!.color,
-        );
+        value = _getNextValue(value);
       });
     }
   }
@@ -96,18 +91,180 @@ class _DiceWidgetState extends State<DiceWidget> {
   void _onVerticalDragEnd(DragEndDetails details) {
     if (details.velocity.pixelsPerSecond.dy > 0) {
       setState(() {
-        _dice = Dice(
-          value: _dice!.value,
-          color: _getPreviousColor(_dice!.color),
-        );
+        color = _getPreviousColor(color);
       });
     } else {
       setState(() {
-        _dice = Dice(
-          value: _dice!.value,
-          color: _getNextColor(_dice!.color),
-        );
+        color = _getNextColor(color);
       });
     }
+  }
+}
+
+class _DotsStackWidget extends StatelessWidget {
+  const _DotsStackWidget({
+    required this.size,
+    required this.diameter,
+    required this.count,
+  }) : radius = diameter / 2;
+
+  final double size;
+  final double diameter;
+  final double radius;
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: _createDotsForValue(count),
+    );
+  }
+
+  List<Widget> _createDotsForValue(int value) {
+    switch (value) {
+      case 1:
+        return [
+          Positioned(
+            left: size / 2 - radius,
+            top: size / 2 - radius,
+            child: _DotWidget(diameter: diameter),
+          )
+        ];
+      case 2:
+        return [
+          Positioned(
+            left: size / 4 - radius,
+            top: size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: 3 * size / 4 - radius,
+            top: 3 * size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+        ];
+      case 3:
+        return [
+          Positioned(
+            left: size / 4 - radius,
+            top: size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: size / 2 - radius,
+            top: size / 2 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: 3 * size / 4 - radius,
+            top: 3 * size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+        ];
+      case 4:
+        return [
+          Positioned(
+            left: size / 4 - radius,
+            top: size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: 3 * size / 4 - radius,
+            top: 3 * size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: 3 * size / 4 - radius,
+            top: size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: size / 4 - radius,
+            top: 3 * size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+        ];
+      case 5:
+        return [
+          Positioned(
+            left: size / 4 - radius,
+            top: size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: 3 * size / 4 - radius,
+            top: 3 * size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: 3 * size / 4 - radius,
+            top: size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: size / 4 - radius,
+            top: 3 * size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: size / 2 - radius,
+            top: size / 2 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+        ];
+      case 6:
+        return [
+          Positioned(
+            left: size / 4 - radius,
+            top: size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: 3 * size / 4 - radius,
+            top: 3 * size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: 3 * size / 4 - radius,
+            top: size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: size / 4 - radius,
+            top: 3 * size / 4 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: size / 4 - radius,
+            top: size / 2 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+          Positioned(
+            left: 3 * size / 4 - radius,
+            top: size / 2 - radius,
+            child: _DotWidget(diameter: diameter),
+          ),
+        ];
+      default:
+        return [];
+    }
+  }
+}
+
+class _DotWidget extends StatelessWidget {
+  const _DotWidget({required this.diameter});
+
+  final double diameter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: diameter,
+      height: diameter,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+      ),
+    );
   }
 }
